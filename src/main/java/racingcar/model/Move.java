d2e5d3e61582a8862a4d4d96ed6a1d6b13d2572d
ssamzag <ssamzag@gmail.com>
2022-04-25 19:15:41 +0900

    feat: MovingStrategy 구현체 RandomMove 구현 - 단위테스트용 생성자 추가

--- src/main/java/racingcar/model/Move.java
+++
@@ -1,8 +0,0 @@
-package racingcar.model;
-
-public class Move implements MovingStrategy {
-    @Override
-    public boolean movable() {
-        return false;
-    }
-}
---
+++ src/test/java/racingcar/model/RandomMoveTest.java
@@ -0,0 +1,22 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomMoveTest {
+    @Test
+    @DisplayName("move 값이 3 또는 10이면 false를 반환한다")
+    void movableFalse() {
+        assertThat(new RandomMove(3).movable()).isFalse();
+        assertThat(new RandomMove(10).movable()).isFalse();
+    }
+
+    @Test
+    @DisplayName("move 값이 4 또는 9면 true를 반환한다")
+    void movableTrue() {
+        assertThat(new RandomMove(4).movable()).isTrue();
+        assertThat(new RandomMove(9).movable()).isTrue();
+    }
+}
